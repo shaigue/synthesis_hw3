efba96a95279c4d30283ee4439a50674d29239f6
abialystok <asaf.b@campus.technion.ac.il>
2021-06-17 18:51:20 +0300

    No need to pass env to wp function

--- src/while_lang/weakest_precondition.py
+++ src/while_lang/weakest_precondition.py
@@ -27,28 +27,27 @@ def parse_expr(expr: Tree, env):
                          parse_expr(expr.subtrees[1], env))
 
 
-def weakest_precondition(c: Tree, Q, env):
+def weakest_precondition(c: Tree, Q, linv):
     # should not be bool, but a formula in z3 -NOT BOOL!!
     if c.root == "skip":
         return Q
     elif c.root == ":=":
         to_replace = c.terminals[0]
-        expr = parse_expr(c.subtrees[1], env)
-        return lambda e: Q(upd(e, to_replace, expr))
+        return lambda e: Q(upd(e, to_replace, parse_expr(c.subtrees[1], e)))
     elif c.root == ";":
-        Q2 = weakest_precondition(c.subtrees[1], Q, env)
-        return weakest_precondition(c.subtrees[0], Q2, env)
+        Q2 = weakest_precondition(c.subtrees[1], Q, linv)
+        return weakest_precondition(c.subtrees[0], Q2, linv)
     elif c.root == "if":
         then_clause, else_clause = c.subtrees[1], c.subtrees[2]
         return lambda e: Or(And(parse_expr(c.subtrees[0], e), weakest_precondition(then_clause, Q, e)),
                             And(Not(parse_expr(c.subtrees[0], e)), weakest_precondition(else_clause, Q, e)))
     elif c.root == "while":
         body = c.subtrees[1]
-        P = env["linv"]
-        vars = [env[k] for k in env.keys() if k != "linv"]
-        return lambda e: And(P(e), ForAll(vars, And(Implies(And(P(e), parse_expr(c.subtrees[0], e)),
-                                                            weakest_precondition(body, P, e)(e)),
-                                                    Implies(And(P(e), Not(parse_expr(c.subtrees[0], e))), Q(e)))))
+        P = linv
+        return lambda e: And(P(e), ForAll([e[k] for k in e.keys()],
+                                          And(Implies(And(P(e),parse_expr(c.subtrees[0], e)),
+                                                      weakest_precondition(body, P, e)(e)),
+                                              Implies(And(P(e), Not(parse_expr(c.subtrees[0], e))), Q(e)))))
 
 
 if __name__ == "__main__":
@@ -58,6 +57,5 @@ if __name__ == "__main__":
     linv = lambda d: d['a'] == d['b']
     ast = WhileParser()(program)
     env = {term: Int(term) for term in ast.terminals if isinstance(term, str)}
-    env["linv"] = linv
-    wp = weakest_precondition(ast, Q, env)(env)
+    wp = weakest_precondition(ast, Q, linv)(env)
     print(wp)
